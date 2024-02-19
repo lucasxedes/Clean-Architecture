ab7f06183e73e97309669b7a32904293944ebef1
prxs <prxs@pop-os.localdomain>
2024-02-19 17:41:15 -0300

    feat: Implementing UsersRepository

--- src/infra/db/settings/connection.py
+++ src/infra/db/settings/connection.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 class DBConnectionHandler:
 
@@ -7,11 +8,12 @@ class DBConnectionHandler:
             'mysql+pymysql',
             'root',
             'root',
-            '172.17.0.2',
+            'localhost',
             '3306',
             'clean_database'
         )
         self.__engine = self.__create_database_engine()
+        self.session = None
 
     def __create_database_engine(self):
         engine = create_engine(self.__connection_string)
@@ -19,3 +21,11 @@ class DBConnectionHandler:
 
     def get_engine(self):
         return self.__engine
+
+    def __enter__(self):
+        session_maker = sessionmaker(bind=self.__engine)
+        self.session = session_maker()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close()
